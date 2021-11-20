dc2a24e8d651cf0bc6d3a351f3e2cf276ebc261d
Steven Villarosa <steven.s.villarosa@gmail.com>
2021-11-20 18:51:36 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 30;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dfe598f9-63a1-474a-b2f9-f61663b9b15f';
+export const MODAL_CLOSE_SEC = 2.5;
